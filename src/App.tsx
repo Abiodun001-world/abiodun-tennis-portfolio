b16356979866271d656ab2835519e53d475cad0a
Abiodun Adekunle <94719889+Abiodun001-world@users.noreply.github.com>
2026-04-29 22:56:44 +0100

    Update App.tsx to import and render Booking component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AICoach from './components/AICoach';
+import Booking from './components/Booking';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <AICoach />
+      <Booking />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
